02649b85c9fbd2078b4a4445f8831915468a617b
Arjun Tanwar <arjunthakur900@gmail.com>
2023-04-09 12:47:48 +0530

    useMutate

--- src/components/RQSuperheroes.page.tsx
+++ src/components/RQSuperheroes.page.tsx
@@ -1,32 +1,45 @@
-import { useSuperhero } from "../hooks/useSuperheroes"
-import { useState } from 'react'
+import { useSuperhero, useAddSuperHeroData } from "../hooks/useSuperheroes"
+import { FormEvent, useState } from 'react'
 import { Link } from 'react-router-dom'
 
 export default function RQSuperheroesPage() {
-
-
-
+    
     const onSuccess = () => {
         console.log('Data fetched')
     }
     const onError = () => {
         console.log('Error occurred')
     }
-
-    const { data, error, isLoading, isFetching } = useSuperhero(onSuccess, onError)
-
-    console.log({ isLoading, isFetching })
+    
+    const [hero, setHero] = useState({ name: '', alterEgo: ''})
+    const { mutate } = useAddSuperHeroData()
+    const { data, error, isLoading } = useSuperhero(onSuccess, onError)    
 
     if(isLoading) return <div>Loading...</div>
     else if(error instanceof Error) return <div>{error.message}</div>
+    
+    const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+        event.preventDefault()
+        mutate(hero)
+    }
 
-    return <ul>
-
-        {data?.map((val, index) => {
-            return <li className = 'text-gray-600' key = {index}>
-                <Link to={`${val.id}`}>{val.name}</Link>
-                <br />
-            </li>
-        })}
-    </ul>
+    return <div>
+        <form onSubmit = {handleSubmit}>    
+            <input className = 'border-2 border-gray-300 mt-1 rounded-md ml-1' value = {hero.name} onChange = {event => {
+                setHero({ name: event.currentTarget.value, alterEgo: hero.alterEgo })
+            }} />
+            <input className = 'border-2 border-gray-300 mt-1 rounded-md ml-1' value = {hero.alterEgo} onChange = {event => {
+                setHero({ name: hero.name, alterEgo: event.currentTarget.value })
+            }} />
+            <button>Add Hero</button>
+        </form>
+        <ul>
+            {data?.map((val, index) => {
+                return <li className = 'text-gray-600' key = {index}>
+                    <Link to={`${val.id}`}>{val.name}</Link>
+                    <br />
+                </li>
+            })}
+        </ul>
+    </div>
 }
--- src/hooks/useSuperheroes.ts
+++ src/hooks/useSuperheroes.ts
@@ -1,4 +1,4 @@
-import { useQuery } from "@tanstack/react-query"
+import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query"
 import { HeroType } from "../components/Superhero.page"
 
 const fetchData: () => Promise<HeroType[]> = async () => {
@@ -6,9 +6,23 @@ const fetchData: () => Promise<HeroType[]> = async () => {
         const res = await fetch('http://localhost:4000/superheroes')
         if(!res.ok) return Promise.reject('Whoops!')
         return res.json() as Promise<HeroType[]>
-        
     } catch (error) {
         throw new Error('Whoops!')
+    }
+}
+
+const addHero = async (hero: { name: string, alterEgo: string }): Promise<HeroType> => {
+    try {
+        const res = await fetch('http://localhost:4000/superheroes', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(hero)
+        })
+        return res.json()
+    } catch (error) {
+        throw new Error("Post failed")
     }
 }
 
@@ -18,5 +32,20 @@ export const useSuperhero = (onSuccess: () => void, onError: () => void) => {
         queryFn: fetchData,
         onSuccess: () => onSuccess,
         onError: () => onError
+    })
+}
+
+export const useAddSuperHeroData = () => {
+    const queryClient = useQueryClient()
+    return useMutation({
+        mutationFn: addHero,
+        // onSuccess: () => {
+        //     queryClient.invalidateQueries(['superheroes'])
+        // }
+        onSuccess: (data) => {
+            queryClient.setQueriesData<HeroType[]>(['superheroes'], (prev) => {
+                return [...prev as HeroType[], data]
+            })
+        }
     })
 }
